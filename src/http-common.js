64fe326060bc3aa81a7bf98bec49d6a541342f26
joaorobjr <joaorobjr@gmail.com>
2020-11-11 21:03:02 +0000

    remove dotenv config

--- src/http-common.js
+++ src/http-common.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
-}
 //Define a URL base da origem para consumo do servico
 export default axios.create({
   //baseURL: 'http://localhost:3000/',

f3f56a2d9ca19dfc2796d1864b6cda9cd78772af
joaorobjr <joaorobjr@gmail.com>
2020-11-11 21:01:14 +0000

    set url api endpoint

--- src/http-common.js
+++ src/http-common.js
@@ -6,7 +6,8 @@ if (process.env.NODE_ENV !== 'production') {
 }
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: process.env.API_ENDPOINT,
+  //baseURL: 'http://localhost:3000/',
+  baseURL: 'https://module4-grades-api.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
